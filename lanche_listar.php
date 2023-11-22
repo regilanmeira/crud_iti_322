e89a209dcf93507158c465c094c95dcc3e194427
Regilan Meira <regilan@hotmail.com>
2023-11-22 09:40:15 -0300

    Adicionei o botão VER e a página para exibir um lanche

--- lanche_listar.php
+++ lanche_listar.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar lanches</title>
     <link rel="stylesheet" href="estilo.css">
+
+    <script src="https://kit.fontawesome.com/aa1f2d8e44.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <h1>Lanches disponiveis</h1>
@@ -23,6 +25,7 @@
         <tr>
             <td>Descrição</td>
             <td>Preço</td>
+            <td>Ações</td>
         </tr>
 
         <?php
@@ -32,6 +35,11 @@
             <tr>
                 <td><?php echo $linha["descricao"]; ?></td>
                 <td><?php echo $linha["preco"]; ?></td>
+                <td>
+                    <a href="lanche_ver.php?id_lanche=<?php echo $linha["id_lanche"]; ?>&descricao=<?php echo $linha["descricao"] ?>">
+                      <i class="fa-solid fa-eye fa-beat" style="color: #1d918f;"> </i>
+                    </a>
+            </td>
             </tr>
         <?php 
             }
